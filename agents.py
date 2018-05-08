16ede32af85ddbf2c4c438ee4818702f386bbdbe
Andrew Drozdov <andrew@mrdrozdov.com>
2018-05-07 23:04:43 -0400

    Receiver with tests.

--- agents.py
+++ agents.py
@@ -12,12 +12,19 @@ class Agent(nn.Module):
 
 
 class AgentConfig(object):
-    message_in = 32
-    message_out = 256
+    message_size = 32
+    sender_hidden_dim = 256
+    receiver_hidden_dim = 256
+    receiver_score_dim = 1
+    descriptor_dim = 100
+    message_in = message_size
+    message_out = sender_hidden_dim
     image_in = 512
-    image_out = 256
-    binary_in = 256
-    binary_out = 32
+    image_out = sender_hidden_dim
+    binary_in = sender_hidden_dim
+    binary_out = message_size
+    stop_size = 1
+    nclasses = 30
 
 
 class Sender(Agent):
@@ -26,6 +33,7 @@ class Sender(Agent):
 
         self.config = config
 
+        # Network for communication
         self.message_bias = nn.Parameter(torch.FloatTensor(config.message_out))
         self.message_layer = nn.Linear(config.message_in, config.message_out)
         self.image_layer = nn.Linear(config.image_in, config.image_out)
@@ -59,3 +67,99 @@ class Sender(Agent):
             outp_msg = torch.round(dist).detach()
 
         return outp_msg, dist
+
+
+class Receiver(Agent):
+    def __init__(self, config):
+        super(Receiver, self).__init__()
+
+        self.config = config
+
+        # RNN Network
+        self.rnn = nn.GRUCell(config.message_size, config.receiver_hidden_dim)
+        # Network for decisions
+        self.stop_layer = nn.Linear(config.receiver_hidden_dim, config.stop_size)
+        # Network for class predicitons
+        self.y1 = nn.Linear(config.receiver_hidden_dim + config.descriptor_dim, config.receiver_hidden_dim)
+        self.y2 = nn.Linear(config.receiver_hidden_dim, config.receiver_score_dim)
+        # Network for communication
+        self.state_layer = nn.Linear(config.receiver_hidden_dim, config.receiver_hidden_dim)
+        self.descriptor_layer = nn.Linear(config.descriptor_dim, config.receiver_hidden_dim, bias=False)
+        self.message_layer = nn.Linear(config.receiver_hidden_dim, config.message_size)
+
+    def build_state_descriptor_pairs(self, state, descriptors):
+        batch_size, state_size = state.size()
+        ndescriptors, descriptor_size = descriptors.size()
+
+        pair_left = state.unsqueeze(2)\
+            .expand(batch_size, state_size, ndescriptors)\
+            .contiguous()\
+            .view(batch_size * ndescriptors, -1)
+
+        pair_right = descriptors.unsqueeze(0)\
+            .expand(batch_size, ndescriptors, descriptor_size)\
+            .contiguous()\
+            .view(batch_size * ndescriptors, -1)
+
+        pair = torch.cat([pair_left, pair_right], dim=1)
+
+        return pair
+
+    def reweight_descriptors(self, scores, descriptors):
+        batch_size = scores.size(0)
+        ndescriptors, descriptor_size = descriptors.size()
+
+        scores_broadcast = scores.unsqueeze(2)\
+            .expand(batch_size, ndescriptors, descriptor_size)
+        descriptors_broadcast = descriptors.unsqueeze(0)\
+            .expand(batch_size, ndescriptors, descriptor_size)
+        reweighted = (scores_broadcast * descriptors_broadcast).sum(1).squeeze(1)
+
+        return reweighted
+
+    def forward(self, message, state, image, class_descriptors):
+        batch_size, state_size = state.size()
+        ndescriptors, descriptor_size = class_descriptors.size()
+
+        # Encode history.
+        new_state = self.rnn(message, state)
+
+        # Predict stop bit.
+        stop_dist = F.sigmoid(self.stop_layer(new_state))
+        if self.training:
+            # TODO: Use built-in torch sampling.
+            stop_dist_ = stop_dist.detach().cpu().numpy()
+            stop_bit = torch.from_numpy((np.random.rand(*stop_dist_.shape) < stop_dist_).astype('float32'))
+        else:
+            stop_bit = torch.round(stop_dist).detach()
+
+        # Predict classes.
+
+        ## Expand for pairwise scoring.
+        pairs = self.build_state_descriptor_pairs(new_state, class_descriptors)
+
+        ## Predict classes.
+        y = self.y1(pairs).clamp(min=0)
+        y = self.y2(y).view(batch_size, -1)
+
+        # Obtain communications
+
+        ## Reweight descriptions based on current model confidence.
+        y_scores = F.softmax(y, dim=1).detach()
+        descriptor_influence = self.reweight_descriptors(y_scores, class_descriptors)
+
+        ## Hidden state for Receiver message.
+        message_state = F.tanh(self.state_layer(new_state) + self.descriptor_layer(descriptor_influence))
+
+        ## Message distribution.
+        message_dist = F.sigmoid(self.message_layer(message_state))
+
+        ## Generate output message.
+        if self.training:
+            # TODO: Use built-in torch sampling.
+            message_dist_ = message_dist.detach().cpu().numpy()
+            outp_msg = torch.from_numpy((np.random.rand(*message_dist_.shape) < message_dist_).astype('float32'))
+        else:
+            outp_msg = torch.round(message_dist).detach()
+
+        return (stop_bit, stop_dist), (outp_msg, message_dist), y
--- tests/test_agents.py
+++ tests/test_agents.py
@@ -2,8 +2,18 @@ import os
 import unittest
 
 import torch
+import torch.nn.functional as F
 
-from agents import AgentConfig, Sender
+from agents import AgentConfig, Sender, Receiver
+
+
+def check_bernoulli_out(self, out):
+    self.assertEqual(out.eq(0).sum().item() + out.eq(1).sum(), out.numel())
+
+
+def check_bernoulli_dist(self, dist):
+    self.assertTrue(dist.le(1).sum().item() == dist.numel() \
+        and dist.ge(0).sum().item(), dist.numel())
 
 
 class TestSender(unittest.TestCase):
@@ -35,6 +45,73 @@ class TestSender(unittest.TestCase):
         self.assertEqual(dist.le(1).sum().item(), dist.numel())
         self.assertEqual(dist.ge(0).sum().item(), dist.numel())
         self.assertEqual(out_msg.eq(0).sum().item() + out_msg.eq(1).sum(), out_msg.numel())
+
+
+class TestReceiver(unittest.TestCase):
+    def setUp(self):
+        self.config = AgentConfig()
+
+    def test_forward_train(self):
+        batch_size = 3
+        receiver = Receiver(self.config)
+        receiver.train()
+
+        msg = torch.FloatTensor(batch_size, self.config.message_in).normal_()
+        state = torch.FloatTensor(batch_size, self.config.receiver_hidden_dim).fill_(0)
+        descriptors = torch.FloatTensor(self.config.nclasses, self.config.descriptor_dim).normal_()
+        (stop_bit, stop_dist), (message, message_dist), y = receiver(msg, state, None, descriptors)
+
+        check_bernoulli_dist(self, stop_dist)
+        check_bernoulli_out(self, stop_bit)
+        check_bernoulli_dist(self, message_dist)
+        check_bernoulli_out(self, message)
+        self.assertEqual(y.size(), (batch_size, self.config.nclasses))
+
+    def test_forward_eval(self):
+        batch_size = 3
+        receiver = Receiver(self.config)
+        receiver.eval()
+
+        msg = torch.FloatTensor(batch_size, self.config.message_in).normal_()
+        state = torch.FloatTensor(batch_size, self.config.receiver_hidden_dim).fill_(0)
+        descriptors = torch.FloatTensor(self.config.nclasses, self.config.descriptor_dim).normal_()
+        (stop_bit, stop_dist), (message, message_dist), y = receiver(msg, state, None, descriptors)
+
+        check_bernoulli_dist(self, stop_dist)
+        check_bernoulli_out(self, stop_bit)
+        check_bernoulli_dist(self, message_dist)
+        check_bernoulli_out(self, message)
+        self.assertEqual(y.size(), (batch_size, self.config.nclasses))
+
+    def test_build_pairs(self):
+        receiver = Receiver(self.config)
+
+        state = torch.FloatTensor(3, 2)
+        state[0] = 0
+        state[1] = 1
+        state[2] = 2
+        descs = torch.FloatTensor(2, 4)
+        descs[0] = 5
+        descs[1] = 6
+
+        pairs = receiver.build_state_descriptor_pairs(state, descs)
+
+        self.assertEqual(pairs.size(), (6, 6))
+        self.assertTrue(pairs[[0, 1], :2].eq(0).all())
+        self.assertTrue(pairs[[2, 3], :2].eq(1).all())
+        self.assertTrue(pairs[[4, 5], :2].eq(2).all())
+        self.assertTrue(pairs[[0, 2, 4], 2:].eq(5).all())
+        self.assertTrue(pairs[[1, 3, 5], 2:].eq(6).all())
+
+    def test_reweight_descriptors(self):
+        receiver = Receiver(self.config)
+
+        batch_size = 2
+        scores = F.softmax(torch.FloatTensor(batch_size, self.config.nclasses).normal_(), dim=1)
+        descriptors = torch.FloatTensor(self.config.nclasses, self.config.descriptor_dim).normal_()
+        influence = receiver.reweight_descriptors(scores, descriptors)
+
+        self.assertEqual(influence.size(), (batch_size, self.config.descriptor_dim))
 
 
 if __name__ == '__main__':
